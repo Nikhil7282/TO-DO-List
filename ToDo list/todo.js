c89356f054ca276b2b8f9753db5cc7c8d8476738
Nikhil7282 <112640546+Nikhil7282@users.noreply.github.com>
2022-09-08 08:56:01 -0700

    Add files via upload

---
+++ ToDo list/todo.js
@@ -0,0 +1,21 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener("click",function(){
+    var para=document.createElement('p');
+    para.innerText=inputField.value;
+    toDoContainer.appendChild(para);
+    inputField.value= "";
+
+    
+
+
+
+    para.addEventListener("click",function(){
+        para.style.textDecoration="line-through";
+    })
+    para.addEventListener("dblclick",function(){
+        toDoContainer.removeChild(para);
+    })
+})
